255010422f7dad6df36091d4960abd55bacdac7e
kookou <nnaangee@gmail.com>
2020-10-27 19:00:35 +0900

    합치는중

--- ui/src/components/MainPage/Main.jsx
+++ ui/src/components/MainPage/Main.jsx
@@ -27,17 +27,6 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const sections = [
-    { title: '메인', url: '/main' },
-    { title: '리뷰보기', url: '/review' },
-    { title: '리뷰쓰기', url: '/reviewwrite' },
-    { title: '마이페이지', url: '/userpage' },
-    { title: 'Opinidddon', url: '#' },
-    { title: 'Science', url: '#' },
-    { title: 'Health', url: '#' },
-    { title: 'Style', url: '#' },
-    { title: 'Travel', url: '#' },
-];
 
 const shoplistl = [
     {
