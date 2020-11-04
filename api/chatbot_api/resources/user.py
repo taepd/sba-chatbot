75180c01a81b0176f88aebc63cf38670d6cda508
taepd <taepd1@gmail.com>
2020-11-04 09:21:06 +0900

    fix flask-session trouble

--- api/chatbot_api/resources/user.py
+++ api/chatbot_api/resources/user.py
@@ -3,6 +3,7 @@
 from pdb import main
 from typing import List
 from flask import request, session
+from flask_cors import cross_origin
 from flask_restful import Resource, reqparse
 from flask import jsonify
 import json
@@ -104,9 +105,9 @@ class UserDao(UserDto):
     def find_by_id(cls, userid):
         return cls.query.filter_by(userid == userid).first()
 
+    # @cross_origin(supports_credentials=True)
     @classmethod
-    def login(cls, user):
-        session['userid'] = user.userid
+    def login(cls, user):  
         sql = cls.query\
             .filter(cls.userid.like(user.userid))\
             .filter(cls.password.like(user.password))
@@ -589,6 +590,8 @@ class Access(Resource):
         print(user.userid)
         print(user.password)
         data = UserDao.login(user)
+        if data[0]:
+            session['userid'] = user.userid  
         return data[0], 200
 
 
